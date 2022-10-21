4586d3d4458a88db9bd7172fd2e9b19530e4b7b7
Andrew Obrigewitsch <blazestudios23@gmail.com>
2022-10-20 21:42:37 -0700

    Code added to get AbortController to work.

--- src/lambdas/breeds-get.ts
+++ src/lambdas/breeds-get.ts
@@ -1,8 +1,7 @@
 import fetch from 'node-fetch'
 
+import AbortController from 'abort-controller'
 import { Response, ErrorResponse } from './types'
-
-const { AbortController } = globalThis
 
 export interface DogResponse extends Response {
   body: {
@@ -20,11 +19,30 @@ const DOG_BREEDS_ENDPOINT = 'https://dog.ceo/api/breeds/'
 
 export async function handler(): Promise<DogResponse | ErrorResponse> {
   const controller = new AbortController()
+  const { signal } = controller
+  signal.addEventListener('abort', () => {
+    console.log('aborted!')
+  })
+
+  signal.addEventListener('onabort', () => {
+    console.log('aborted dang it!')
+  })
+
   const timeout = setTimeout(() => {
     controller.abort()
   }, 150)
+
   try {
-    const res = await fetch(`${DOG_BREEDS_ENDPOINT}list/all`, { signal: controller.signal })
+    const res = await fetch(`${DOG_BREEDS_ENDPOINT}list/all`, { signal })
+
+    if (signal.onabort) {
+      throw new Error('timeout')
+    }
+
+    console.log('anything in signal', signal)
+    if (!res) {
+      throw new Error('No data returned!')
+    }
     const payload: DogAPI = await res.json()
 
     const dogs = payload.message
@@ -40,7 +58,13 @@ export async function handler(): Promise<DogResponse | ErrorResponse> {
       statusCode: 200,
       body: { ...payload, message },
     }
-  } catch (error: unknown) {
+  } catch (error: any) {
+    console.log('signal: ', signal)
+    console.log('error: ', error)
+
+    if (signal.aborted) {
+      console.log('Operation tifmed out')
+    }
     // implementing error handling but get an error that AbortError doesn't exist in node fetch
     // if (error instanceof AbortError) {
     //   return {
